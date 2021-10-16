da1d477b281bbc0868c8f4a44d605fe815426187
kusshp <kushal.2025patil@gmail.com>
2021-10-16 22:55:12 +0530

    Test Case 6

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,7 +2,7 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
+    public int add(String input) throws Exception {
         //commit attempt 3
     	if(input.isEmpty()) {
     		return 0;
@@ -14,14 +14,31 @@ class StringCalculator {
     	String[] nums = input.split("[,|\n]+");
     	
     	if(!input.isEmpty() && nums.length == 2) {
-    		return Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
+    		return getSum(nums); //Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
     	}
     	
     	if(!input.isEmpty() && nums.length == 3) {
-    		System.out.println(nums[0]+nums[1]+nums[2]);
-    		return Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]) + Integer.parseInt(nums[2]);
+    		//System.out.println(nums[0]+nums[1]+nums[2]);
+    		return getSum(nums); //Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]) + Integer.parseInt(nums[2]);
     	}
     	return 0;
     }
+
+	private int getSum(String[] nums) throws Exception {
+		for(String s : nums) {
+			if(Integer.parseInt(s) < 0) {
+				throw new Exception("negatives not allowed");
+			}
+		}
+		int sum = 0;
+		for(String s : nums) {
+			if(Integer.parseInt(s) > 1000) {
+				continue;
+			}
+			
+			sum += Integer.parseInt(s);
+		}
+		return sum;
+	}
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -8,29 +8,39 @@ class StringCalculatorShould {
 
 	StringCalculator stringCalculator = new StringCalculator();
     @Test
-    void empty_string_should_return_0() {
+    void empty_string_should_return_0() throws Exception {
         //StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    void string_with_single_number_should_return_number_as_int() {
+    void string_with_single_number_should_return_number_as_int() throws Exception {
        // StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
     
     @Test
-    void string_should_return_sum() {
+    void string_should_return_sum() throws Exception {
     	assertEquals(3, stringCalculator.add("1,2"));
     }
     
     @Test
-    void newLine_between_two_numbers_return_sum() {
+    void newLine_between_two_numbers_return_sum() throws Exception {
     	assertEquals(3, stringCalculator.add("1\n,2"));
     }
     
     @Test
-    void newLine_between_three_numbers_return_sum() {
+    void newLine_between_three_numbers_return_sum() throws Exception {
     	assertEquals(6, stringCalculator.add("1\n,2,\n3"));
+    }
+    
+    @Test
+    void negative_input_return_exception() throws Exception {
+    	stringCalculator.add("-5");
+    }
+    
+    @Test
+    void ignore_number_greater_than_1000() throws Exception {
+    	assertEquals(2, stringCalculator.add("2,1001"));
     }
 }
